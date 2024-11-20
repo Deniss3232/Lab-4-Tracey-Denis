e6aa336ad6e567597bd7d85a1d82a6c94d3e6c91
Deniss3232 <iqdenis@gmail.com>
2024-11-19 21:44:54 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+/*.
+ * Creado por: Tracey Dally Ramirez Barcarcel y  Denis Roberto Rodríguez Jiménez 
+ * Fecha de creación: 08/11/2024
+ * Última modificación: 17/11/2024
+ * 
+ * Descripción:
+ * BMW desea un sistema de climatización polimórfico que funcione para tres clases distintas de vehículos entre ellos se encuentran los siguientes: 
+ * Clase A(Gama alta), Clase B(Gama media) y la Clase C (Gama básica).
+ *  La funcionalidad debe de ser flexible para que se puedan intercambiar las clases sin reprogramación. 
+ * Este sistema debe ser desarrollado usando interfaces las cuales garanticen el principio de Segregación de interfaces
+ *  y que estos cumplan con los requisitos específicos de cada clase,
+ *  los cuales serán detallados más adelante.
+ * 
+ * Créditos:
+ * Este programa ha sido inspirado y desarrollado a partir de conocimientos
+obtenidos en diversas fuentes
+ * de programación en Java, incluyendo documentación oficial de Java.
+ * Se han consultado las siguientes fuentes:
+ * - Documentación oficial de Oracle sobre Java:
+https://docs.oracle.com/javase/tutorial/
+ * - Ejemplos y guías de Stack Overflow: https://stackoverflow.com/
+ */
+// Main.java
+
+public class Main {
+    public static void main(String[] args) {
+        MainWindow.main(args); // Ejecuta la interfaz gráfica
+    }
+}
